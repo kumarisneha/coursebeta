7851a1f33f95c008bdc5acdc1427440569442e68
kumarisneha <nav@ratna123>
2020-07-01 19:29:53 +0530

    added application folder

---
+++ coursebeta/urls.py
@@ -0,0 +1,45 @@
+"""coursebeta URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# from django.contrib import admin
+# from django.urls import path
+# from django.conf.urls import url,include
+# from courseapp import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     url(r'^$',views.index,name='index'),
+#     url(r'^courseapp/',include('courseapp.urls')),
+#     url(r'^logout/$', views.user_logout, name='logout'),
+# ]
+
+
+
+from django.contrib import admin 
+from django.urls import path, include 
+from courseapp import views as user_view 
+from django.contrib.auth import views as auth 
+  
+urlpatterns = [ 
+  
+    path('admin/', admin.site.urls), 
+  
+    ##### user related path##########################  
+    path('', include('courseapp.urls')), 
+    path('login/', user_view.Login, name ='login'), 
+    path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'), 
+    path('register/', user_view.register, name ='register'),
+  
+] 
