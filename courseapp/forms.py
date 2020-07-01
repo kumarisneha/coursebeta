98b6b4de3c4ddd0f19015934b82f9e58f53ba08e
kumarisneha <nav@ratna123>
2020-07-01 19:26:19 +0530

    added models for course creation

--- courseapp/forms.py
+++ courseapp/forms.py
@@ -1,9 +1,22 @@
 from django import forms 
 from django.contrib.auth.models import User 
 from django.contrib.auth.forms import UserCreationForm 
-  
-  
-  
+from .models import CourseCreate
+
+COURSE_TYPE_CHOICE= [
+    ('IT/Software', 'IT/software'),
+    ('Engineering', 'engineering'),
+    ('UPSE', 'upse'),
+    ('SSC', 'ssc'),
+    ]
+
+class CourseCreationForm(forms.ModelForm):
+    course_type= forms.CharField(widget=forms.Select(choices=COURSE_TYPE_CHOICE))
+
+    class Meta:
+        model = CourseCreate
+        fields = '__all__'
+
 class UserRegisterForm(UserCreationForm): 
     email = forms.EmailField() 
     first_name = forms.CharField(max_length = 20) 
--- courseapp/models.py
+++ courseapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+COURSE_TYPE_CHOICE= [
+    ('IT/Software', 'IT/software'),
+    ('Engineering', 'engineering'),
+    ('UPSE', 'upse'),
+    ('SSC', 'ssc'),
+    ]
+
+class CourseCreate(models.Model):
+    course_title = models.CharField(max_length=50)
+    course_type    = models.CharField(max_length=15, choices=COURSE_TYPE_CHOICE)
+    description = models.TextField(max_length=300)
