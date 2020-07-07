1bba06157d2b507bcc8d6c381bbaa948f8f46f1b
kumarisneha <kumarisneha102@gmail.com>
2020-07-08 00:50:55 +0530

    Added admin file

--- courseapp/admin.py
+++ courseapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CourseCreate
+from .models import CourseCreate, SyllabusAddition
 
 # Register your models here.
 
 admin.site.register(CourseCreate)
+admin.site.register(SyllabusAddition)
