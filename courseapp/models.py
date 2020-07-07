3a82d076a5974a1917a9e0e89706e7d0547cd3a6
kumarisneha <kumarisneha102@gmail.com>
2020-07-08 00:51:12 +0530

    Added models file

--- courseapp/models.py
+++ courseapp/models.py
@@ -9,6 +9,11 @@ COURSE_TYPE_CHOICE= [
     ]
 
 class CourseCreate(models.Model):
-    course_title = models.CharField(max_length=50)
-    course_type    = models.CharField(max_length=15, choices=COURSE_TYPE_CHOICE)
+    course_title = models.CharField(max_length=50, unique = True)
+    course_type = models.CharField(max_length=15, choices=COURSE_TYPE_CHOICE)
     description = models.TextField(max_length=300)
+
+class SyllabusAddition(models.Model):
+    topic = models.CharField(max_length=50)
+    link = models.CharField(max_length=250)
+    title = models.ForeignKey(CourseCreate, blank=True, null=True, on_delete=models.SET_NULL)
