b7b4045a701eef05968b8d3e5581529141ce416d
kumarisneha <kumarisneha102@gmail.com>
2020-07-10 02:23:04 +0530

    Changed some code

--- courseapp/views.py
+++ courseapp/views.py
@@ -4,46 +4,87 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login 
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth.forms import AuthenticationForm 
-from .forms import UserRegisterForm, CourseCreationForm
+from .forms import UserRegisterForm, CourseCreationForm, SyllabusAdditionForm
+from .models import SyllabusAddition, CourseCreate
 from django.core.mail import send_mail, EmailMultiAlternatives 
 from django.template.loader import get_template 
 from django.template import Context
 from django.conf import settings 
-   
+from django.http import HttpResponseRedirect
    
 def index(request):
     return render(request, 'user/index.html',{'title':'index'})
 
+def link(request):
+    return render(request, 'user/index.html')
 
-# def home_view(request): 
-#     context = {} 
-#     context['form'] = GeeksForm() 
-#     return render( request, "home.html", context) 
-
-    # if request.method == "POST":
-    #     form = CreationForm(request.POST)
-    #     if form.is_valid():
-    #         create = form.save()
-    #         return redirect('post_detail', pk=create.pk)
-    # else:
-    #     form = PostForm()
-    # return render(request, 'blog/post_edit.html', {'form': form})
 def create_course(request):
     context = {} 
     context['form'] = CourseCreationForm()
-    if request.method == "POST": 
+    if request.method == "POST":
+        print("inside-------------------")
         form = CourseCreationForm(request.POST)
+        print(form.is_valid)
+        print("form---------", form)
+        course_title = request.POST['course_title']
+        course_type = request.POST['course_type']
+        description = request.POST['description']
         if form.is_valid():
-            course_title = request.POST['course_title'] 
-            course_type = request.POST['course_type'] 
-            description = request.POST['description']
             form.save()
-            print(course_title, course_type, description) 
-            return render(request, 'submit_course.html')
+            obj = CourseCreate.objects.get(course_title = course_title)
+            context['course_title'] = course_title
+            context['course_type'] = course_type
+            context['description'] = description
+            context['obj'] = obj
+            print(course_title, course_type, description, request.POST)
+            print(context)
+            # return redirect('add_topic', id=obj.id)
+            # return HttpResponseRedirect(reverse('add_topic', kwargs={'id': obj.id}))
+            return HttpResponseRedirect('/add_topic/%d' % obj.id)
+        else:
+
+            obj = CourseCreate.objects.get(course_title = course_title)
+            print(form.errors['course_title'].as_data())
+            print ("objjjjjjjjjjj", obj)
+            # print(form.item.errors.as_text())
+            for t in form.errors['course_title'].as_data():
+                context['text'] = t
+                print(type(t))
+
+            # [u'This field is required.']
+            # if obj.course_title == course_title:
+            #     t = "Course title already exists. Please give another title"
+            #     context['text'] = t
+            #     print(context)
+            #     print(form.errors)
+            return render(request,'create_course.html', context)
+
+    print("outside-------------------")
     return render(request, 'create_course.html',context)
 
-########################################################################
-########### register here #####################################
+def add_topic(request, title_id):
+    print("------------",title_id)
+    context = {}
+    context['form'] = SyllabusAdditionForm()
+    context['title_id'] = title_id
+    context['add_syllabus'] = SyllabusAddition.objects.filter(title_id=title_id)
+    if request.method == "POST":
+        form = SyllabusAdditionForm(request.POST)
+        if form.is_valid():
+            course_topic = request.POST['topic']
+            course_link = request.POST['link']
+            instance = form.save(commit=False)
+            print(request.session)
+            print(form)
+            instance.title_id = title_id
+            instance.save()
+            print("instance----------",instance)
+            print(course_topic, course_link)
+            context['add_syllabus'] = SyllabusAddition.objects.filter(title_id=title_id)
+            # obj = CourseCreate.objects.get(course_title = course_title)
+            print("coooooooooo",context)
+            return render(request, 'submit_course.html', context)
+    return render(request, 'submit_course.html', context)
 
 def register(request):
     if request.method == 'POST':
@@ -64,12 +105,14 @@ def register(request):
             try:
                 # send_mail( subject, html_content, from_email, [to] )
                 msg.send()
+                print("Mail sent successfully")
             except Exception as e:
                 print(e)
                 print("error in sending mail")
             ##################################################################
             form.save()
             username = form.cleaned_data.get('username')
+            print(username)
             messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
